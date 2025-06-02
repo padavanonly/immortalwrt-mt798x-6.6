aad576c3135630011ffbac70430f192d736ba9f9
Starktomy <37720599+Starktomy@users.noreply.github.com>
2025-06-03 02:13:17 +0800

    kernel: modules: package driver for MediaTek T7xx PCIe 5G modem
    Build and package driver for MediaTek PCIe 5G WWAN modem T7xx device
    available in Linux 6.1 and 6.6 as kmod-mtk-t7xx.

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -1412,3 +1412,21 @@ define KernelPackage/mhi-wwan-mbim/description
 endef
 
 $(eval $(call KernelPackage,mhi-wwan-mbim))
+
+define KernelPackage/mtk-t7xx
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=MediaTek T7xx 5G modem
+  DEPENDS:=@PCI_SUPPORT +kmod-wwan
+  KCONFIG:=CONFIG_MTK_T7XX
+  FILES:=$(LINUX_DIR)/drivers/net/wwan/t7xx/mtk_t7xx.ko
+  AUTOLOAD:=$(call AutoProbe,mtk_t7xx)
+endef
+
+define KernelPackage/mtk-t7xx/description
+  Enables MediaTek PCIe based 5G WWAN modem (T7xx series) device.
+  Adapts WWAN framework and provides network interface like wwan0
+  and tty interfaces like wwan0at0 (AT protocol), wwan0mbim0
+  (MBIM protocol), etc.
+endef
+
+$(eval $(call KernelPackage,mtk-t7xx))
